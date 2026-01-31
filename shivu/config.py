ad6f894bdfe625758778b9c39b14a1b457977fcd
Piyush <piyush960yogi@gmail.com>
2026-01-31 23:07:55 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -32,7 +32,7 @@ class Config:
     # Database
     MONGO_URL: str = os.getenv(
         "MONGO_URL",
-        "mongodb+srv://ravi:ravi12345@cluster0.hndinhj.mongodb.net/?retryWrites=true&w=majority"
+        "mongodb+srv://Alisha:Alisha123@cluster0.yqcpftw.mongodb.net/?retryWrites=true&w=majority"
     )
 
     # Media
